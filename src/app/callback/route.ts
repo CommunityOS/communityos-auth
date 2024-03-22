874eb1ab12186a9d76b35206e0f85f3ea4fdc8ce
Felipe Torres <ftorres@openai.com>
2024-03-22 13:12:00 -0700

    update

--- src/app/callback/route.ts
+++ src/app/callback/route.ts
@@ -50,3 +50,4 @@ export async function GET(request: Request) {
     return NextResponse.json(error);
   }
 }
+export const runtime = 'edge';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,54 @@
-import Link from 'next/link';
+import { WorkOS } from '@workos-inc/node';
+import { getUser, signOut } from './auth';
 
-export default function Home() {
+// This example uses Next.js with React Server Components.
+// Because this page is an RSC, the code stays on the server, which allows
+// us to use the WorkOS Node SDK without exposing our API key to the client.
+//
+// If your application is a single page app (SPA), you will need to:
+// - create a form that can POST to an endpoint in your backend
+// - call the `getAuthorizationURL` method in that endpoint
+// - redirect the user to the returned URL
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY);
+
+export default async function WithSession() {
+  const { isAuthenticated, user } = await getUser();
+
+  const authKitUrl = workos.userManagement.getAuthorizationUrl({
+    clientId: process.env.WORKOS_CLIENT_ID || '',
+    provider: 'authkit',
+    redirectUri: 'http://localhost:3000/using-hosted-authkit/with-session/callback',
+  });
+
   return (
     <main>
-      <h1>Examples</h1>
-      <ul>
-        <li>
-          <Link href="/using-your-own-ui">Using your own UI</Link>
-        </li>
-        <li>
-          <Link href="/using-hosted-authkit">Using hosted AuthKit</Link>
-        </li>
-      </ul>
+      <h1>With session</h1>
+
+      {isAuthenticated ? (
+        <>
+          <h2>Welcome back{user?.firstName && `, ${user?.firstName}`}</h2>
+          <p>You are now authenticated into the application.</p>
+
+          <form
+            action={async () => {
+              'use server';
+              await signOut();
+            }}
+          >
+            <button type="submit">Sign-out</button>
+          </form>
+        </>
+      ) : (
+        <>
+          <h2>Sign-in</h2>
+          <p>Sign-in to view your account details</p>
+          <a href={authKitUrl}>Sign-in</a>
+        </>
+      )}
+
+      <pre>{JSON.stringify({ user }, null, 2)}</pre>
     </main>
   );
 }
+export const runtime = 'edge';
